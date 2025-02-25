d96e6f684264335bf29d562c0b5e0e343e2e7eaf
trupal godhat <trupal12@gmail.com>
2025-02-25 15:20:06 +0530

    testing

--- src/component/Profile/Profile.jsx
+++ src/component/Profile/Profile.jsx
@@ -16,7 +16,7 @@ function Profile() {
               <div className={styles.img_container}>
                 <img src={profile_pic} alt="" />
               </div>
-              <div>
+              <div className={styles.width_auto}>
                 <p className={styles.name}>Trupal Godhat</p>
                 <p className={styles.username}>@trupal1211</p>
                 <p className={styles.tags}>
@@ -30,6 +30,12 @@ function Profile() {
             <div className={styles.follow_btn}>
               follow
             </div>
+
+            {/* <div className={`${styles.follow_btn} ${styles.edit}`}>
+              edit  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="14" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+                    <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325"/></svg>
+            </div> */}
+
           </div>
 
           <div className={styles.bio_container}>
